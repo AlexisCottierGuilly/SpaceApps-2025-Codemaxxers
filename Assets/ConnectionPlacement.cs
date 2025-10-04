152cd85d23544281bec24b33547f082d66820126
Alexis CG <113210633+AlexisCottierGuilly@users.noreply.github.com>
2025-10-04 19:40:47 -0400

    Knob changes

--- Assets/ConnectionPlacement.cs
+++ Assets/ConnectionPlacement.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.Animations;
+using System.Collections.Generic;
 
 public class ConnectionPlacement : MonoBehaviour
 {
@@ -46,9 +48,12 @@ public class ConnectionPlacement : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             ConnectionManager clickedObject = GetPressedConnectionKnob();
+
             Debug.Log("Clicked on: " + (clickedObject != null ? clickedObject.substance.ToString() : "Nothing"));
             if (clickedObject != null)
             {
+                clickedObject.GetComponent<Animator>().SetTrigger("Click");
+
                 if (selectedConnectionKnob == null)
                 {
                     selectedConnectionKnob = clickedObject;
@@ -57,13 +62,12 @@ public class ConnectionPlacement : MonoBehaviour
                 {
                     if (selectedConnectionKnob != clickedObject &&
                     selectedConnectionKnob.connectionType != clickedObject.connectionType &&
-                    (selectedConnectionKnob.substance == clickedObject.substance 
+                    (selectedConnectionKnob.substance == clickedObject.substance
                     || selectedConnectionKnob.substance == Substance.Any || clickedObject.substance == Substance.Any))
                     {
                         Debug.Log("Connected " + selectedConnectionKnob.substance + " to " + clickedObject.substance);
-
-                        // create a simple line between the two connection knobs
                         CreateConnectionLine(selectedConnectionKnob, clickedObject);
+                        selectedConnectionKnob = null;
                     }
                 }
             }
@@ -81,17 +85,6 @@ public class ConnectionPlacement : MonoBehaviour
 
         GameObject connection = Instantiate(connectionPrefab, connectionsParent.transform);
         LineRenderer lineRenderer = connection.GetComponent<LineRenderer>();
-        lineRenderer.positionCount = 2;
-        Vector3 pos1 = start.transform.position;
-        pos1.z = 1;
-        Vector3 pos2 = end.transform.position;
-        pos2.z = 1;
-
-        lineRenderer.SetPosition(0, pos1);
-        lineRenderer.SetPosition(1, pos2);
-
-        lineRenderer.startColor = GameManager.instance.colors.GetColor(start.substance);
-        lineRenderer.endColor = GameManager.instance.colors.GetColor(end.substance);
 
         Connection connectionClass = connection.GetComponent<Connection>();
         connectionClass.sourceProcess = start.parentProcess;
@@ -107,6 +100,51 @@ public class ConnectionPlacement : MonoBehaviour
         end.isConnected = true;
 
         connection.SetActive(true);
+
+        UpdateConnectionLine(connection);
+    }
+
+    void UpdateConnectionLine(GameObject connection)
+    {
+        // Only update line position
+        LineRenderer lineRenderer = connection.GetComponent<LineRenderer>();
+        Connection connectionClass = connection.GetComponent<Connection>();
+        ConnectionManager start = connectionClass.sourceProcess.outputConnections[connectionClass.sourceProductIndex].GetComponent<ConnectionManager>();
+        ConnectionManager end = connectionClass.targetProcess.inputConnections[connectionClass.targetReactantIndex].GetComponent<ConnectionManager>();
+
+        List<Vector3> positions = GetLinePositions(start.transform.position, end.transform.position);
+        lineRenderer.positionCount = positions.Count;
+        lineRenderer.SetPositions(positions.ToArray());
+
+        lineRenderer.startColor = GameManager.instance.colors.GetColor(start.substance);
+        lineRenderer.endColor = GameManager.instance.colors.GetColor(end.substance);
+    }
+
+    List<Vector3> GetLinePositions(Vector3 start, Vector3 end)
+    {
+        // all the z pos should be 1.
+        // we want to get 1: start, 2: same level as start and midpoint with end, 3: same level as end and midpoint with start, 4: end
+
+        float diffY = Mathf.Abs(start.y - end.y);
+
+        if (diffY < 0.75f)
+        {
+            return new List<Vector3> { start, end };
+        }
+
+        float minX = Mathf.Min(start.x, end.x);
+        float maxX = Mathf.Max(start.x, end.x);
+
+        float xDistance = maxX - minX;
+        minX = minX + 0.25f * xDistance;
+        maxX = maxX - 0.25f * xDistance;
+
+        float x = Random.Range(minX, maxX);
+
+        Vector3 pos1 = new Vector3(x, start.y, 1);
+        Vector3 pos2 = new Vector3(x, end.y, 1);
+
+        return new List<Vector3> { start, pos1, pos2, end };
     }
     
     Vector2 GetMouseWorldPosition()
